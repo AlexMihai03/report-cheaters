ed195bea2188c46a1d3d493258cf555130552d9f
AlexMihai03 <alexmihai.03@gmail.com>
2020-06-19 11:55:28 +0300

    basic running server

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import cors from "cors";
+import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
+
+const app = express();
+
+app.use(cors());
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => [
+      {
+        title: "Harry Potter and the Chamber of Secrets",
+        author: "J.K. Rowling",
+      },
+      {
+        title: "Jurassic Park",
+        author: "Michael Crichton",
+      },
+    ],
+  },
+};
+
+const server = new ApolloServer({
+  introspection: true,
+  playground: true,
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app, path: "/graphql" });
+
+app.listen({ port: 8000 }, () => {
+  console.log("[Server] Apollo Server on http://localhost:8000/graphql");
+});
