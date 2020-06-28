e566550d6772dc85453d65e8affdc1126d43ec89
AlexMihai03 <alexmihai.03@gmail.com>
2020-06-28 12:59:55 +0300

    added provision tenant

--- src/identityaccess/src/main/model/identity/TenantProvisioningService.ts
+++ src/identityaccess/src/main/model/identity/TenantProvisioningService.ts
@@ -1,7 +1,14 @@
-import { Either } from "../../../../../common/core/Result";
+import { Either, Result, left, right } from "../../../../../common/core/Result";
 import { Tenant } from "./Tenant";
 import { ITenantRepository } from "./TenantRepository";
 import { IUserRepository } from "./UserRepository";
+import { AppError } from "@common/core/AppError";
+
+type Response = Either<
+  AppError.UnexpectedError |
+  Result<any>,
+  Result<Tenant>
+>;
 
 export class TenantProvisioningService {
   private tenantRepository: ITenantRepository;
@@ -15,14 +22,67 @@ export class TenantProvisioningService {
   public provisionTenant(
     tenantName: string,
     tenantDescription: string,
-  ) {
+    administratorUsername: string,
+    emailAddress: string,
+  ): Response {
+    try {
+      const tenantOrError = Tenant.create(
+        {
+          name: tenantName,
+          description: tenantDescription,
+          active: true,
+          registrationInvitations: new Set(),
+        },
+        this.tenantRepository.nextIdentity(),
+      );
 
+      if (tenantOrError.isFailure) {
+        return left(
+          Result.fail<Tenant>(tenantOrError.error.toString()),
+        ) as Response;
+      }
+
+      const tenant = tenantOrError.getValue();
+
+      this.tenantRepository.add(tenant);
+
+      this.registerAdministratorFor(
+        tenant,
+        administratorUsername,
+        emailAddress,
+      );
+
+      tenant.publishTenantProvisioned(tenant.id);
+
+      return right(Result.ok<Tenant>(tenant));
+    } catch (err) {
+      return left(new AppError.UnexpectedError(err));
+    }
   }
 
   private registerAdministratorFor(
     tenant: Tenant,
-    // administratorName: FullName,
+    adminUsername: string,
+    emailAddress: string,
   ): void {
+    const invitation =
+      tenant.offerRegistrationInvitation("init")
+            .getValue()
+            .openEnded();
 
+    const strongPassword =
+      DomainRegistry
+        .passwordService()
+        .generateStrongPassword();
+
+    const adminOrError = tenant.registerUser(
+      invitation.invitationId,
+      "admin",
+      strongPassword,
+      adminUsername,
+      emailAddress,
+    );
+
+    this.userRepository.add(adminOrError.getValue());
   }
 }
